174020f83bcdd824476b66271036fec35ed8aabc
Eduardo Rodighero <91704510+EduRodighero@users.noreply.github.com>
2026-04-06 18:23:59 -0300

    Create tarefa_2.py

---
+++ exercicios_decisao/tarefa_2.py
@@ -0,0 +1,8 @@
+# Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+valor_teste = float(input('Digite um valor: '))
+
+if valor_teste >= 0:
+    print('Positivo')
+else:
+    print('Negativo')
