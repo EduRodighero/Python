d66050c5ec904d04371bb90a42af2d14187f1944
Eduardo Rodighero <91704510+EduRodighero@users.noreply.github.com>
2026-04-06 18:10:19 -0300

    Create exc_4.py

---
+++ exercicios_sequencial/exc_4.py
@@ -0,0 +1,8 @@
+# Faça um programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
+gruas_f = float (input ('Digite os Graus em Fahrenheit: '))
+
+
+graus_c = 5 * ((gruas_f - 32) / 9)
+
+print('O graus em Celcius é: ', graus_c)
